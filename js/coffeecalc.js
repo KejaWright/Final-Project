96a8d3b536e5279262edb9511c8bc2ee13c5a1ca
drewtuan <76626471+drewtuan@users.noreply.github.com>
2023-11-20 16:33:06 -0500

    Add files via upload

--- js/coffeecalc.js
+++ js/coffeecalc.js
@@ -1,46 +1,126 @@
-$('#start-button').click(function(){
-    $('#game').toggle();
-    $('#start-button').toggle();
-/////OBJECTS AND STUFF FOR THE GAME///////
-///VERY BASIC SETUP///
-const canvas = document.getElementById("game");
-if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-    //left box for orders
-    ctx.fillStyle = "#946B34";
-    ctx.fillRect(0, 50, 200, 500);
-    //right box for ingredients
-    ctx.fillStyle = "#583D1A";
-    ctx.fillRect(800, 50, 200, 500);
-    //top box for score and stuff
-    ctx.fillStyle = "#FF9300";
-    ctx.fillRect(0, 0, 1000, 50);
-    //bottom box for blend types
-    ctx.fillStyle = "#FF9300";
-    ctx.fillRect(200, 450, 600, 100);
-    
+//reserved for login/signup
+
+// password field
+var password_field = document.getElementById("password");
+var username_field = document.getElementById("username");
+
+// create account button
+var create_account_button = document.getElementById("create-account-button");
+
+// get the user's password
+function getPassword() {
+  var mypassword = password_field.value;
+  return mypassword;
 }
 
-//Order display
+// get the user's username
+function getUsername() {
+  var myusername = username_field.value;
+  return myusername;
+}
+
+// function that checks the username length to make sure it is in the minimum length and maximum length requirements.
+function checkUsernameLength() {
+  let username_min_length = parseInt(username_field.getAttribute("minlength"));
+  let username_max_length = parseInt(username_field.getAttribute("maxlength"));
+
+  if (getUsername().length < username_min_length || getUsername().length > username_max_length) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// function that validates the password length to make sure it is in the minimum length and maximum length requirements.
+function checkPasswordLength() {
+
+  var password_field_min_length = parseInt(password_field.getAttribute("minlength"));
+  var password_field_max_length = parseInt(password_field.getAttribute("maxlength"));
+
+  if (getPassword().length < password_field_min_length || getPassword().length > password_field_max_length) {
+    return false;
+  } else {
+    return true;
+  }
+
+}
+
+// function that checks the password to make sure it matches the pattern.
+function makeSureThePasswordMatchesPattern() {
+
+  let password_pattern = password_field.getAttribute("pattern");
+  if(getPassword().match(password_pattern)) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+// function that checks to make sure the username matches the pattern.
+function makeSureTheUsernameMatchesPattern() {
+  let username_pattern = username_field.getAttribute("pattern");
+  if(getUsername().match(username_pattern)) {
+    console.log("Username good");
+    return true;
+  } else {
+    console.log("username bad");
+    return false;
+  }
+}
+
+// this function clears the textboxes in the form
+function clearTextboxes() {
+  password_field.value = "";
+  username_field.value = "";
+}
+
+// this function shows on the form labels that the player's credentials are valid
+function showThatPlayerCredentialsAreValid() {
+  window.alert("Account successfully created! Now login with your new password and enjoy the game!");
+}
+
+// this function checks to make sure that the password has the right length and matches the pattern
+function checkpasswordRequirements() {
+  if(makeSureThePasswordMatchesPattern() && checkPasswordLength()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// this function checks to make sure that the username has the right length and matches the pattern
+function checkusernameRequirements() {
+  if(makeSureTheUsernameMatchesPattern() && checkUsernameLength()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// this function validates both the username and the password in order to find out if the user's credentials
+// can be pushed into a database.
+function checkUsernameAndPasswordForDatabase() {
+  if(checkpasswordRequirements() && checkusernameRequirements()) {
+    console.log("true");
+    showThatPlayerCredentialsAreValid();
+    return true;
+  } else {
+    console.log("false");
+    return false;
+  }
+}
+
+// register event handler to create account button to create an account if the account credentials are valid.
+create_account_button.addEventListener("click", checkUsernameAndPasswordForDatabase, false);
 
 
-////Ingredients Display
+
+
+
+//reserved for delete button
 
 
 
 
-////Blend options menu Display
-
-
-
-////Top menu bar
-//
-
-
-
-
-
-
-//////MATH AND STUFF FOR THE GAME//////
-////Adding ingredients
-})
+//reserved for coffee game
