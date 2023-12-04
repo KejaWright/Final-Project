41140ebcfaa26fc087fc6981dfb5f7c349963e6a
drewtuan <76626471+drewtuan@users.noreply.github.com>
2023-12-03 20:54:33 -0500

    Add files via upload

--- js/coffeecalc.js
+++ js/coffeecalc.js
@@ -1,126 +1,25 @@
-//reserved for login/signup
+// put data in database after all the playing is done
+//rtghy@gmail.com 123werZXS;']
 
-// password field
-var password_field = document.getElementById("password");
-var username_field = document.getElementById("username");
+const firebaseConfig = {
 
-// create account button
-var create_account_button = document.getElementById("create-account-button");
+  apiKey: "AIzaSyD3xx7nx749QdAVXwvKw6V4IRIAlTUO-Lk",
 
-// get the user's password
-function getPassword() {
-  var mypassword = password_field.value;
-  return mypassword;
-}
+  authDomain: "csci-225-final-project-a-bb0d1.firebaseapp.com",
 
-// get the user's username
-function getUsername() {
-  var myusername = username_field.value;
-  return myusername;
-}
+  projectId: "csci-225-final-project-a-bb0d1",
 
-// function that checks the username length to make sure it is in the minimum length and maximum length requirements.
-function checkUsernameLength() {
-  let username_min_length = parseInt(username_field.getAttribute("minlength"));
-  let username_max_length = parseInt(username_field.getAttribute("maxlength"));
+  storageBucket: "csci-225-final-project-a-bb0d1.appspot.com",
 
-  if (getUsername().length < username_min_length || getUsername().length > username_max_length) {
-    return false;
-  } else {
-    return true;
-  }
-}
+  messagingSenderId: "691914384953",
 
-// function that validates the password length to make sure it is in the minimum length and maximum length requirements.
-function checkPasswordLength() {
+  appId: "1:691914384953:web:b910332ffe97e93ef50fe8",
 
-  var password_field_min_length = parseInt(password_field.getAttribute("minlength"));
-  var password_field_max_length = parseInt(password_field.getAttribute("maxlength"));
+  measurementId: "G-W41900LN38"
 
-  if (getPassword().length < password_field_min_length || getPassword().length > password_field_max_length) {
-    return false;
-  } else {
-    return true;
-  }
-
-}
-
-// function that checks the password to make sure it matches the pattern.
-function makeSureThePasswordMatchesPattern() {
-
-  let password_pattern = password_field.getAttribute("pattern");
-  if(getPassword().match(password_pattern)) {
-    return true;
-  } else {
-    return false;
-  }
-  
-}
-
-// function that checks to make sure the username matches the pattern.
-function makeSureTheUsernameMatchesPattern() {
-  let username_pattern = username_field.getAttribute("pattern");
-  if(getUsername().match(username_pattern)) {
-    console.log("Username good");
-    return true;
-  } else {
-    console.log("username bad");
-    return false;
-  }
-}
-
-// this function clears the textboxes in the form
-function clearTextboxes() {
-  password_field.value = "";
-  username_field.value = "";
-}
-
-// this function shows on the form labels that the player's credentials are valid
-function showThatPlayerCredentialsAreValid() {
-  window.alert("Account successfully created! Now login with your new password and enjoy the game!");
-}
-
-// this function checks to make sure that the password has the right length and matches the pattern
-function checkpasswordRequirements() {
-  if(makeSureThePasswordMatchesPattern() && checkPasswordLength()) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// this function checks to make sure that the username has the right length and matches the pattern
-function checkusernameRequirements() {
-  if(makeSureTheUsernameMatchesPattern() && checkUsernameLength()) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// this function validates both the username and the password in order to find out if the user's credentials
-// can be pushed into a database.
-function checkUsernameAndPasswordForDatabase() {
-  if(checkpasswordRequirements() && checkusernameRequirements()) {
-    console.log("true");
-    showThatPlayerCredentialsAreValid();
-    return true;
-  } else {
-    console.log("false");
-    return false;
-  }
-}
-
-// register event handler to create account button to create an account if the account credentials are valid.
-create_account_button.addEventListener("click", checkUsernameAndPasswordForDatabase, false);
+};
 
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-
-
-//reserved for delete button
-
-
-
-
-//reserved for coffee game
